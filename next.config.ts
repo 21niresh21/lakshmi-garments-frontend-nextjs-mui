1b061961854dcf5cad25513548c15cb45ba045dd
Niresh K <k.niresh@in.bosch.com>
2025-12-18 09:56:58 +0530

    disabled webpack

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* existing config options */
   reactCompiler: true,
-
-  // Enable SVG as React components
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
